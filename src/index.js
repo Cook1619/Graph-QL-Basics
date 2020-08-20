fa9326a477d467a1f9eed4571ecd0e18346232af
Cook1619 <cook6348@gmail.com>
2020-08-20 15:38:09 -0500

    Setting up graphql with yoga server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("testing");
+import { GraphQLServer } from "graphql-yoga";
+
+//Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "Hello World!!";
+    },
+    name() {
+      return "Hello Matt!!";
+    },
+    location() {
+      return "Mayer, MN";
+    },
+    bio() {
+      return "I'm a fullstack web dev from Minnesota";
+    },
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
