a69e604a97114ffcce4be008d65ad98591b4e5ce
Cook1619 <cook6348@gmail.com>
2020-08-21 15:04:06 -0500

    setting up relational data between data types

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import { GraphQLServer } from "graphql-yoga";
 // Dummy Data
 const users = [
   {
-    id: 1,
+    id: "1",
     name: "Matt",
     email: "cook@example.com",
     age: 33,
   },
   {
-    id: 2,
+    id: "2",
     name: "Danelle",
     email: "danelle@example.com",
   },
   {
-    id: 3,
+    id: "3",
     name: "Jules",
     email: "jules@example.com",
     age: 6,
@@ -23,28 +23,32 @@ const users = [
 
 const posts = [
   {
-    id: 1,
+    id: "1",
     title: "Javascript Post",
     body: "This is a post on why JS is so awesome and unqiue",
     published: true,
+    author: "1",
   },
   {
-    id: 2,
+    id: "2",
     title: "CSS Post",
     body: "This is a post on why CSS is so awesome and difficult",
     published: true,
+    author: "1",
   },
   {
-    id: 3,
+    id: "3",
     title: "React Post",
     body: "This is a post on why React is so performant and awesome",
     published: true,
+    author: "2",
   },
   {
-    id: 4,
+    id: "4",
     title: "NextJS Post",
     body: "This is a post on why NextJS is so awesome and unqiue",
     published: true,
+    author: "3",
   },
 ];
 
@@ -61,37 +65,27 @@ const typeDefs = `
       name: String!
       email: String!
       age: Int
+      posts: [Post!]!
   }
   type Post {
       id: ID!
       title: String!
       body: String!
       published: Boolean!
+      author: User!
+  }
+  type Comment {
+      
   }
 `;
 
 const resolvers = {
   Query: {
-    me() {
-      return {
-        id: "abc123",
-        name: "Matt",
-        email: "cook@example.com",
-      };
-    },
-    post() {
-      return {
-        id: "123nbc",
-        title: "This is a post title",
-        body:
-          "This is the body of the post which has more words than the title",
-        published: true,
-      };
-    },
     users(parent, args, ctx, info) {
       if (!args.query) {
         return users;
       }
+
       return users.filter((user) => {
         return user.name.toLowerCase().includes(args.query.toLowerCase());
       });
@@ -100,11 +94,44 @@ const resolvers = {
       if (!args.query) {
         return posts;
       }
+
       return posts.filter((post) => {
-        return (
-          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
-          post.body.toLowerCase().includes(args.query.toLowerCase())
-        );
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        return isTitleMatch || isBodyMatch;
+      });
+    },
+    me() {
+      return {
+        id: "123098",
+        name: "Mike",
+        email: "mike@example.com",
+      };
+    },
+    post() {
+      return {
+        id: "092",
+        title: "GraphQL 101",
+        body: "",
+        published: false,
+      };
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+  },
+  User: {
+    posts(parent, args, cxt, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
       });
     },
   },
